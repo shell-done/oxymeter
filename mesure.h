6528389487d8562ba249a4241169e84215d3b512
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-30 17:39:20 +0200

    Complete Mesure functions

--- mesure.h
+++ mesure.h
@@ -2,6 +2,19 @@
 #define MESURE_H
 
 #include "define.h"
+#include "fichiers.h"
+
+typedef enum e_EtatMesure {
+    PIC_HAUT, PIC_BAS
+} e_EtatMesure;
+
+typedef struct s_MesureParams {
+    float minAcr, maxAcr;
+    float minAcir, maxAcir;
+    int nbEch;
+
+    e_EtatMesure etat;
+} s_MesureParams;
 
 /** 
  * Fonctions
