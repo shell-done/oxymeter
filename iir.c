c42634e4e8c589ca07be17393ecc4df9a1c696f2
Dankov Memevo <thomas.brague@netcourrier.fr>
2020-03-30 17:40:50 +0200

    Filtrage IIR (test unitaire non fonctionnel)

--- iir.c
+++ iir.c
@@ -1,7 +1,63 @@
 #include "iir.h"
 
-absorp iirTest(char* filename){
+s_IIRParameters* initIIR() {
+	s_IIRParameters* parameters = malloc(sizeof(s_IIRParameters));
+
+	parameters->lastacrX = 0;
+	parameters->lastacrY = 0;
+	parameters->lastacirX = 0;
+	parameters->lastacirY = 0;
+}
+
+void iir(s_IIRParameters* parameters, absorp* myAbsorp) {
+	/* Valeurs actuelles */
+	float currentacrY;
+	float currentacrX;
+	float currentacirY;
+	float currentacirX;
+
+	/* Lecture dans myAbsorp */
+	currentacrX = myAbsorp->acr;
+	currentacirX = myAbsorp->acir;
+	
+	/* Application de IIR */
+	currentacrY = currentacrX - parameters->lastacrX + (COEFF_ALPHA * parameters->lastacrY);
+	currentacirY = currentacirX - parameters->lastacirX + (COEFF_ALPHA * parameters->lastacirY);
+
+	/* Place les valeurs calculées dans lastX et lastY */
+	parameters->lastacrX = currentacrX;
+	parameters->lastacrY = currentacrY;
+	parameters->lastacirX = currentacirX;
+	parameters->lastacirY = currentacirY;
+
+	/* Remplace les valeurs deans myAbsorp */
+	myAbsorp->acr = currentacrY;
+	myAbsorp->acir = currentacirY;
+}
+
+absorp iirTest(char* filename) {
+	int etat=0;
 	absorp	myAbsorp;
+
+	s_IIRParameters* parameters = initIIR();
+	FILE* file = initFichier(filename);
+
+	while(etat != EOF) {
+		absorp tmpAbsorp = lireFichier(file, &etat);
+
+		if(etat != EOF) {
+			myAbsorp = tmpAbsorp;
+			iir(parameters, &myAbsorp);
+		}
+	}
+
+	endIIR(parameters);
+	finFichier(file);
 	
 	return myAbsorp;
+}
+
+
+void endIIR(s_IIRParameters* parameters) {
+	free(parameters);
 }
--- iir.h
+++ iir.h
@@ -2,10 +2,23 @@
 #define IIR_H
 
 #include "define.h"
+#include "fichiers.h"
+
+#define COEFF_ALPHA 0.992
+
+typedef struct IIRParameters {
+    float lastacrX;
+    float lastacrY;
+    float lastacirX;
+    float lastacirY;
+} s_IIRParameters;
 
 /** 
  * Fonctions 
  */
+s_IIRParameters* initIIR();
+void iir(s_IIRParameters* parameters, absorp* myAbsorp);
 absorp iirTest(char* filename);
+void endIIR(s_IIRParameters* parameters);
 
 #endif // IIR_H
