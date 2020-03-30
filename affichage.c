5cb800c6b347ed6937e2d2043947fc62b53bec94
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-30 15:33:20 +0200

    Complete Affichage functions

--- affichage.c
+++ affichage.c
@@ -1,5 +1,44 @@
 #include "affichage.h"
 
-void affichage(oxy myOxy){
-	(void)myOxy;
+void createLock() {
+	FILE* lock = fopen(LOCK_FILE_NAME, "w");
+	
+	if(lock == NULL) {
+		printf("dans affichage.c : erreur création fichier %s\n", LOCK_FILE_NAME);
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(lock);
+}
+
+void deleteLock() {
+	remove(LOCK_FILE_NAME);
+}
+
+int lockExists() {
+	return (access(LOCK_FILE_NAME, F_OK) != -1);
+}
+
+void writeData(oxy* myOxy) {
+	FILE* data = fopen(DATA_FILE_NAME, "w");
+
+	if(data == NULL) {
+		printf("dans affichage.c : erreur création fichier %s\n", DATA_FILE_NAME);
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(data, "%d\n%d", myOxy->spo2, myOxy->pouls);
+
+	fclose(data);
+	printf("aa\n");
+}
+
+void affichage(oxy myOxy) {
+	if(!lockExists()) {
+		createLock();
+		writeData(&myOxy);
+		deleteLock();
+	} else {
+		printf("Attente...");
+	}
 }
--- affichage.h
+++ affichage.h
@@ -3,9 +3,16 @@
 
 #include "define.h"
 
+#define DATA_FILE_NAME "Data.txt"
+#define LOCK_FILE_NAME ".verrouData"
+
 /** 
  * Fonctions 
  */
+void createLock();
+void deleteLock();
+int lockExists();
+void writeData(oxy* myOxy);
 void affichage(oxy myOxy);
 
 #endif /* AFFICHAGE_H */
