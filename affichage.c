78b1b084559b15113809cdcf3c9632e7f8132fa6
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-31 11:16:32 +0200

    Comment Affichage files

--- affichage.c
+++ affichage.c
@@ -1,8 +1,11 @@
 #include "affichage.h"
 
+/* Créer le fichier de lock */
 void createLock() {
+	/* Créer le fichier de lock s'il n'existe pas*/
 	FILE* lock = fopen(LOCK_FILE_NAME, "w");
 	
+	/* Vérification qu'il n'y a pas d'erreur à la création du fichier */
 	if(lock == NULL) {
 		printf("dans affichage.c : erreur création fichier %s\n", LOCK_FILE_NAME);
 		exit(EXIT_FAILURE);
@@ -11,33 +14,44 @@ void createLock() {
 	fclose(lock);
 }
 
+/* Supprime le fichier de lock */
 void deleteLock() {
 	remove(LOCK_FILE_NAME);
 }
 
+/* Vérifie si le fichier LOCK_FILE_NAME existe */
 int lockExists() {
+	/*Si access(LOCK_FILE_NAME, F_OK) == -1 le fichier n'existe pas, sinon il existe*/
 	return (access(LOCK_FILE_NAME, F_OK) != -1);
 }
 
+/* Ecrit les informations de myOxy dans le fichier DATA_FILE_NAME */
 void writeData(oxy* myOxy) {
+	/* Créer le fichier data s'il n'existe pas et le prépare en lecture*/
 	FILE* data = fopen(DATA_FILE_NAME, "w");
 
+	/* Vérification qu'il n'y a pas d'erreur à la création du fichier */
 	if(data == NULL) {
 		printf("dans affichage.c : erreur création fichier %s\n", DATA_FILE_NAME);
 		exit(EXIT_FAILURE);
 	}
 
+	/* Ecrit le SpO2 et le poul dans le fichier */
 	fprintf(data, "%d\n%d", myOxy->spo2, myOxy->pouls);
 
 	fclose(data);
 }
 
+/* Ecrit les informations de myOxy dans le fichier DATA_FILE_NAME avec vérification et bloquage du lock */
 void affichage(oxy myOxy) {
+	/* Si le lock n'existe pas */
 	if(!lockExists()) {
+		/* On le créer, on écrit les données puis on le supprime */
 		createLock();
 		writeData(&myOxy);
 		deleteLock();
 	} else {
+		/* Sinon on affiche un message d'attente */
 		printf("Attente...");
 	}
 }
--- affichage.h
+++ affichage.h
@@ -1,18 +1,70 @@
+/*!
+ * \file affichage.h
+ * \brief Fonctions d'affichages
+ * \author Thomas BRAGUE (thomas.brague@isen-ouest.yncrea.fr)
+ * \author Alexandre THOMAS (alexandre.thomas@isen-ouest.yncrea.fr)
+ * \version 1
+ * \date 31/03/2020
+ *
+ * Fonctions liées à la création d'un fichier contenant les valeurs du poul et du SpO2
+ * utilisé pour l'interface Java.
+ *
+ */
+
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
 
 #include "define.h"
 
-#define DATA_FILE_NAME "Data.txt"
-#define LOCK_FILE_NAME ".verrouData"
+#define DATA_FILE_NAME "Data.txt" /* Fichier de destination du poul et de SpO2 */
+#define LOCK_FILE_NAME ".verrouData" /* Fichier bloquant la lecture/écriture du fichier de destination */
 
-/** 
- * Fonctions 
+/*! 
+ * \fn void createLock()
+ * \brief Créer le fichier de lock
+ * 
+ * Créer le fichier défini par LOCK_FILE_NAME bloquant la lecture/écriture de DATA_FILE_NAME
  */
 void createLock();
+
+/*! 
+ * \fn void deleteLock()
+ * \brief Supprime le fichier de lock
+ * 
+ * Supprime le fichier défini par LOCK_FILE_NAME bloquant la lecture/écriture de DATA_FILE_NAME
+ */
 void deleteLock();
+
+/*! 
+ * \fn int lockExists()
+ * \brief Vérifie si le fichier LOCK_FILE_NAME existe
+ * 
+ * \return 1 si le fichier existe, 0 sinon
+ */
 int lockExists();
+
+/*! 
+ * \fn void writeData(oxy* myOxy);
+ * \brief Ecrit les informations de myOxy dans le fichier DATA_FILE_NAME
+ * 
+ * \param myOxy Pointeur sur une structure oxy contenant le poul et le SpO2 a écrire
+ * 
+ * Ecrit le SpO2 (sur la première ligne) et le poul (sur la seconde) dans le fichier DATA_FILE_NAME
+ * pour l'interface Java. Doit être utilisée avec les méthodes createLock(), deleteLock() et lockExists()
+ * afin de vérifier qu'il est bien possible de modifier le fichier et de le bloquer pendant la modification.
+ */
 void writeData(oxy* myOxy);
+
+/*! 
+ * \fn void affichage(oxy* myOxy);
+ * \brief Ecrit les informations de myOxy dans le fichier DATA_FILE_NAME avec vérification et bloquage du lock
+ * 
+ * \param myOxy Pointeur sur une structure oxy contenant le poul et le SpO2 a écrire
+ * 
+ * Ecrit le SpO2 (sur la première ligne) et le poul (sur la seconde) dans le fichier DATA_FILE_NAME
+ * en vérifiant que le fichier de verrou LOCK_FILE_NAME n'est pas présent, et en créant le verrou
+ * pendant sa modification.
+ */
 void affichage(oxy myOxy);
 
 #endif /* AFFICHAGE_H */
