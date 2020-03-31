29103db57a54765fcf8f5ffeb86970e319cb2d1e
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-31 12:07:08 +0200

    Comment Lecture files

--- lecture.c
+++ lecture.c
@@ -1,35 +1,38 @@
 #include "lecture.h"
 
+/* Fonction de lecture d'une ligne du fichier passé en paramètre */
 absorp lecture(FILE* file_pf, int* file_state){
-	absorp myAbsorp = {-1, -1, -1, -1};
-	/**file_state=EOF;*/
+	absorp myAbsorp = {-1, -1, -1, -1}; /* Initialisation de l'absorption en état "invalide" */
+
 	/* Attribue -1 à acr, dcr, acir et dcir afin de vérifier s'il y a bien 4 valeurs dans la ligne lue */
 	int acr=-1, dcr=-1, acir=-1, dcir=-1;
-	int isCorrupted = 0;
+	int isCorrupted = 0; /* Vaut 0 si la ligne n'est pas corrompue, 1 sinon*/
 
 	do {
-		/* Lit une ligne du fichier */
+		/* Lit une ligne du fichier en récupérant les 4 valeurs, séparés par des virgules et se finissant par LFCR */
 		*file_state = fscanf(file_pf, "%d,%d,%d,%d\n\r", &acr, &dcr, &acir, &dcir);
 
 		/* Vérifie que les 4 valeurs acr, dcr, acir et dcir ont bien été lue */
+		/* Si ce n'est pas le cas, isCorrupted = 1 */
 		isCorrupted = (acr == -1 || dcr == -1 || acir == -1 || dcir == -1);
 
+		/* Si on est pas après la dernière ligne */
 		if(*file_state != EOF) {
+			/* Si le fichier est corrompu on affiche un message d'erreur */
 			if(isCorrupted) {
-			/* Affichage en cas de ligne corrompue */
-			printf("Ligne corrompue !\n");
+				printf("Ligne corrompue !\n");
 			} else {
 				/* Attribue les valeurs lues et recentrées aux champs de myAbsorp correspondants */
-				myAbsorp.acr = acr - 2048;
+				myAbsorp.acr = acr - AC_REFOCUS_VALUE;
 				myAbsorp.dcr = dcr;
-				myAbsorp.acir = acir - 2048;
+				myAbsorp.acir = acir - AC_REFOCUS_VALUE;
 				myAbsorp.dcir = dcir;
 			}
 		}
 
+	} while(isCorrupted == 1 && *file_state != EOF); 
+	/* On continue de lire le fichier tant qu'on a pas récupérer une ligne valide et que le fichier n'est pas fini */
 
-	} while(isCorrupted == 1 && *file_state != EOF);
-
-	/* Renvoie myAbsorp rempli de -1 en cas de ligne corrompue et rempli des valeurs lues en cas de ligne valide */
+	/* Renvoie myAbsorp rempli de -1 si EOF et rempli des valeurs lues en cas de ligne valide */
 	return myAbsorp;
 }
--- lecture.h
+++ lecture.h
@@ -1,12 +1,34 @@
+/*!
+ * \file lecture.h
+ * \brief Fonctions liées à la lecture avec prise en compte des erreurs
+ * \author Thomas BRAGUE (thomas.brague@isen-ouest.yncrea.fr)
+ * \author Alexandre THOMAS (alexandre.thomas@isen-ouest.yncrea.fr)
+ * \version 1
+ * \date 31/03/2020
+ *
+ * Fonctions liées à la lecture avec prise en compte des erreurs, simulant
+ * la lecture sur un port USB
+ *
+ */
+
 #ifndef LECTURE_H
 #define LECTURE_H
 
 #include "define.h"
 
-/** 
- * Fonctions
- */
+#define AC_REFOCUS_VALUE 2048 /* Valeur de recentrage des paramètres AC */
 
+/*! 
+ * \fn absorp lecture(FILE* file_pf, int* file_state);
+ * \brief Fonction de lecture d'une ligne du fichier passé en paramètre
+ * 
+ * \param file_pf Un pointeur sur le fichier ouvert à lire
+ * \param file_state Un pointeur sur un entier décrivant le statut de lecture du fichier, vaut EOF si la lecture est terminée
+ * 
+ * \return absorp Les prochaines valeurs d'absorptions non corrompus. Les 4 valeurs sont mises à -1 s'il s'agit de la dernière ligne
+ * 
+ * Lis les prochaines données d'absorption non corrompus depuis le fichier file_pf (doit être ouvert avec fopen auparavant)
+ */
 absorp lecture(FILE* file_pf, int* file_state);
 
 #endif /* LECTURE_H */
