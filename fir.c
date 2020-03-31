e381628bf983f738d94b4b14306526ba0ac8f306
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-31 11:39:47 +0200

    Comment Fir files

--- fir.c
+++ fir.c
@@ -1,65 +1,86 @@
 #include "fir.h"
 
+/* Initialise les paramètres utilisée par les fonctions FIR */
 s_FirParams* initFir() {
 	int i;
+
+	/* Alloue dynamiquement la structure */
 	s_FirParams* params = malloc(sizeof(s_FirParams));
 
+	/* Initialise le buffer circulaire à 0 */
 	for(i=0; i<51; i++) {
 		params->acrSamples[i] = 0;
 		params->acirSamples[i] = 0;
 	}
 
+	/* Initialise l'index courant à 0 */
 	params->currentIdx = 0;
 
 	return params;
 }
 
+/* Ajoute une nouvelle valeur acr/acir dans le buffer circulaire */
 void pushNewValue(s_FirParams* params, absorp* myAbsorb) {
 	params->acrSamples[params->currentIdx] = myAbsorb->acr;
 	params->acirSamples[params->currentIdx] = myAbsorb->acir;
 }
 
+/* Applique la fonction fir aux paramètres acr et acir de myAbsorb */
 void fir(s_FirParams* params, absorp* myAbsorb) {
+	/* Coefficients utilisés pour le filtrage */
 	const float coeffFIR[51] = {1.4774946e-019, 1.6465231e-004, 3.8503956e-004, 7.0848037e-004, 1.1840522e-003, 1.8598621e-003, 2.7802151e-003, 3.9828263e-003, 5.4962252e-003, 7.3374938e-003, 9.5104679e-003, 1.2004510e-002, 1.4793934e-002, 1.7838135e-002, 2.1082435e-002, 2.4459630e-002, 2.7892178e-002, 3.1294938e-002, 3.4578348e-002, 3.7651889e-002, 4.0427695e-002, 4.2824111e-002, 4.4769071e-002, 4.6203098e-002, 4.7081811e-002, 4.7377805e-002, 4.7081811e-002, 4.6203098e-002, 4.4769071e-002, 4.2824111e-002, 4.0427695e-002, 3.7651889e-002, 3.4578348e-002, 3.1294938e-002, 2.7892178e-002, 2.4459630e-002, 2.1082435e-002, 1.7838135e-002, 1.4793934e-002, 1.2004510e-002, 9.5104679e-003, 7.3374938e-003, 5.4962252e-003, 3.9828263e-003, 2.7802151e-003, 1.8598621e-003, 1.1840522e-003, 7.0848037e-004, 3.8503956e-004, 1.6465231e-004, 1.4774946e-019};
-	int i;
+	int i, arrayIdx;
 
+	/* Ajoute les dernières valeurs acr/acir */
 	pushNewValue(params, myAbsorb);
 
+	/* Effectue le calcul de la valeur filtrée */
 	float acr = 0;
 	float acir = 0;
 	for(i = 0; i < 51; i++) {
-		acr += coeffFIR[i]*params->acrSamples[(51 - i + params->currentIdx)%51];
-		acir += coeffFIR[i]*params->acirSamples[(51 - i + params->currentIdx)%51];
+		/* Index à récupérer dans le buffer circulaire */
+		arrayIdx = (51 - i + params->currentIdx)%51;
+
+		acr += coeffFIR[i]*params->acrSamples[arrayIdx];
+		acir += coeffFIR[i]*params->acirSamples[arrayIdx];
 	}
 
+	/* Définit les valeurs ainsi calculées */
 	myAbsorb->acr = acr;
 	myAbsorb->acir = acir;
 
+	/* Passe à l'index suivant du buffer circulaire */
 	params->currentIdx = (params->currentIdx + 1)%51;
 }
 
+/* Libère la mémoire allouée pour la structure params de FIR */
 void closeFir(s_FirParams* params) {
 	free(params);
 }
 
+/* Fonction de test du filtrage FIR */
 absorp firTest(char* filename) {
-	int etat = 0;
+	int etat = 0; /* Etat de la lecture */
 	absorp myAbsorp;
 
-	s_FirParams* params = initFir();
-	FILE* file = initFichier(filename);
+	s_FirParams* params = initFir(); /* Params du FIR */
+	FILE* file = initFichier(filename); /* Fichier contenant les valeurs à filtrer */
 	
-	do {
-		absorp tmpAbsorp = lireFichier(file, &etat);
+	while (etat != EOF) {
+		absorp tmpAbsorp = lireFichier(file, &etat); /* On récupère les données */
 
+		/* Si on est pas en fin de fichier (EOF) */
 		if (etat != EOF) {
+			/* Les valeurs sont valides donc on les filtres */
 			myAbsorp = tmpAbsorp;
 			fir(params, &myAbsorp);
 		}
-	} while (etat != EOF);
+	};
 	
+	/* Désallocation et fermeture */
 	closeFir(params);
 	finFichier(file);
 
+	/* Retourne la dernière absorption filtrée */
 	return myAbsorp;
 }
--- fir.h
+++ fir.h
@@ -1,22 +1,81 @@
+/*!
+ * \file fir.h
+ * \brief Fonctions liées au filtrage FIR
+ * \author Thomas BRAGUE (thomas.brague@isen-ouest.yncrea.fr)
+ * \author Alexandre THOMAS (alexandre.thomas@isen-ouest.yncrea.fr)
+ * \version 1
+ * \date 31/03/2020
+ *
+ * Fonctions liées au filtrage à réponse impulsionnelle finie
+ *
+ */
+
 #ifndef FIR_H
 #define FIR_H
 
 #include "define.h"
 #include "fichiers.h"
 
+/*! 
+ * \struct s_FirParams
+ * \brief Structure contenant les paramètres pour les fonctions FIR
+ */
 typedef struct s_FirParams {
-    float acrSamples[51];
-    float acirSamples[51];
-    int currentIdx;
+    float acrSamples[51]; /*!< Buffer circulaire des 51 dernières valeurs (avant filtrage)*/
+    float acirSamples[51]; /*!< Buffer circulaire des 51 dernières valeurs (avant filtrage)*/
+    int currentIdx; /* Index courant du buffer circulaire (compris entre 0 et 50 inclus) */
 } s_FirParams;
 
-/** 
- * Fonctions 
+/*! 
+ * \fn s_FirParams* initFir()
+ * \brief Initialise les paramètres utilisée par les fonctions FIR
+ * 
+ * Initialise les tableaux à 0 et l'index courant à 0
+ * 
+ * \return Un pointeur sur une sructure s_FirParams allouée dynamiquement
  */
 s_FirParams* initFir();
+
+/*! 
+ * \fn void pushNewValue(s_FirParams* params, absorp* myAbsorb)
+ * \brief Ajoute une nouvelle valeur acr/acir dans le buffer circulaire
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres FIR
+ * \param myAbsorb Un pointeur sur la structure des valeurs d'absorption
+ * 
+ * Remplace la plus ancienne valeur du buffer par la nouvelle et décale de 1
+ * l'index courant
+ */
 void pushNewValue(s_FirParams* params, absorp* myAbsorb);
+
+/*! 
+ * \fn void fir(s_FirParams* params, absorp* myAbsorb)
+ * \brief Applique la fonction fir aux paramètres acr et acir de myAbsorb
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres FIR
+ * \param myAbsorb Un pointeur sur la structure des valeurs d'absorption
+ * 
+ * Applique la fonction fir aux paramètres acr et acir de myAbsorb et ajoute
+ * les nouvelles valeurs aux buffers circulaires
+ */
 void fir(s_FirParams* params, absorp* myAbsorb);
+
+/*! 
+ * \fn void closeFir(s_FirParams* params);
+ * \brief Libère la mémoire allouée pour la structure params de FIR
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres FIR
+ */
 void closeFir(s_FirParams* params);
+
+/*! 
+ * \fn absorp firTest(char* filename);
+ * \brief Fonction de test du filtrage FIR
+ * 
+ * \param filename Le nom du fichier à lire contenant les valeurs à filtrer
+ * 
+ * \return La dernière valeur absorp calculée
+ */
 absorp firTest(char* filename);
 
 #endif /* FIR_H */
