ea9e1aa8ad0c1309badb082c2d49eb000c108708
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-31 11:56:03 +0200

    Comment Integration files

--- integration.c
+++ integration.c
@@ -1,36 +1,47 @@
 #include "integration.h"
 
+/* Fonction de test d'integration des autres fonctions */
 void integrationTest(char* filename) {
-    int etat = 0;
-    int redoMesure = 0;
+    int etat = 0; /* Etat du fichier (vaut EOF si en fin de fichier) */
+    int redoMesure = 0; /* 0: En cours de période, 1: Fin de période, le poul/SpO2 peuvent être recalculés*/
     
-    absorp myAbsorp;
-    oxy myOxy;
+    absorp myAbsorp; /* Absorptions récupérées */
+    oxy myOxy; /* Oxymétrie/Poul calculés */
     
+    /* Initialisation des structure pour les calculs */
     s_FirParams* paramsFir = initFir();
     s_IirParams* paramsIir = initIir();
     s_MesureParams* paramsMesure = initMesure();
     
+    /* Ouverture du fichier */
     FILE* file = initFichier(filename);
     do {
+        /* Lecture d'une ligne */
         myAbsorp = lireFichier(file, &etat);
 
+        /* Si etat != EOF, alors c'est une ligne valide */
         if(etat != EOF) {
+            /* Filtrage des données via FIR et IIR */
             fir(paramsFir, &myAbsorp);
             iir(paramsIir, &myAbsorp);
 
+            /* Vérifie l'avancement dans la période */
             redoMesure = checkPicPosition(paramsMesure, &myAbsorp);
 
+            /* Si une période vient de se finir, on recalcul le poul et le Spo2 */
 			if(redoMesure) {
 				myOxy = mesure(paramsMesure, &myAbsorp);
 			}
 
+            /* Ecriture des données calculées dans le fichier data.txt */
             affichage(myOxy);
         }
     } while (etat != EOF);
 
+    /* Fermeture du fichier */
     finFichier(file);
     
+    /* Désallocation des paramètres utilisés pour les fonctions */
     closeMesure(paramsMesure);
     closeFir(paramsFir);
     closeIIR(paramsIir);
--- integration.h
+++ integration.h
@@ -1,3 +1,15 @@
+/*!
+ * \file integration.h
+ * \brief Fonctions liées au test d'integrations des autres fonctions
+ * \author Thomas BRAGUE (thomas.brague@isen-ouest.yncrea.fr)
+ * \author Alexandre THOMAS (alexandre.thomas@isen-ouest.yncrea.fr)
+ * \version 1
+ * \date 31/03/2020
+ *
+ * Fonctions liées au test d'integrations des fonctions fir, iir, mesure et affichage
+ *
+ */
+
 #ifndef INTEGRATION_H
 #define INTEGRATION_H
 
@@ -8,8 +20,15 @@
 #include "mesure.h"
 #include "affichage.h"
 
-/** 
- * Fonctions
+/*! 
+ * \fn void integrationTest(char* filename)
+ * \brief Fonction de test d'integration des autres fonctions
+ * 
+ * \param filename Le nom du fichier à lire contenant les valeurs à filtrer
+ * 
+ * Lis les données d'absorption depuis le fichier filename puis les filtres avec les fonctions fir et iir.
+ * Des données filtrées sont déduis l'oxymétrie et le poul via la fonction mesure, ces données sont ensuites
+ * écrites dans le fichier Data.txt par la fonction affichage
  */
 void integrationTest(char* filename);
 
