2689122383479bce7b1a806746e78de9d16cd469
Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
2020-03-31 12:41:04 +0200

    Comment Mesure files

--- mesure.c
+++ mesure.c
@@ -1,5 +1,6 @@
 #include "mesure.h"
 
+/* Retourne le maximum entre a et b */
 float maxMesure(float a, float b) {
 	if (a > b) {
 		return a;
@@ -8,6 +9,7 @@ float maxMesure(float a, float b) {
 	}
 }
 
+/* Retourne le minimum entre a et b */
 float minMesure(float a, float b) {
 	if (a < b) {
 		return a;
@@ -16,69 +18,92 @@ float minMesure(float a, float b) {
 	}
 }
 
+/* Initialise les paramètres utilisée par les fonctions de mesure */
 s_MesureParams* initMesure() {
+	/* Alloue dynamiquement la structure */
 	s_MesureParams* params = malloc(sizeof(s_MesureParams));
 
+	/* Initialise les paramètres à 0 */
 	params->minAcr = 0;
 	params->maxAcr = 0;
 	params->minAcir = 0;
 	params->maxAcir = 0;
 	params->nbEch = 0;
+
+	/* On considère qu'on est en début de période */
 	params->etat = PIC_HAUT;
 
 	return params;
 }
 
+/* Modifie la structure params en fonction de l'avancée dans la courbe d'absorption */
 int checkPicPosition(s_MesureParams* params, absorp* absorp) {
-	int doCalculation = 0;
+	int doCalculation = 0; /* Vaut 0 si on est en cours de période, 1 sinon */
 
+	/* Si on est à l'état haut... */
 	if(params->etat == PIC_HAUT) {
+		/* On regarde s'il n'existe pas un "nouveau max" depuis le début de la période */
 		params->maxAcr = maxMesure(params->maxAcr, absorp->acr);
 		params->maxAcir = maxMesure(params->maxAcir, absorp->acir);
 
+		/* Si acr est négatif, c'est qu'il s'agit du milieu de la période, on passe alors en état "PIC_BAS" */
 		if(absorp->acr < 0) {
 			params->etat = PIC_BAS;
 		}
 
 	} else {
+		/* Si on est à l'état bas... */
+		/* On regarde s'il n'existe pas un "nouveau min" depuis le début de la période */
 		params->minAcr = minMesure(params->minAcr, absorp->acr);
 		params->minAcir = minMesure(params->minAcir, absorp->acir);
 
+		/* Si acr est positif, c'est que la période vient de se finir, on passe alors en état "PIC_HAUT" */
 		if(absorp->acr > 0) {
 			params->etat = PIC_HAUT;
 			doCalculation = 1;
 		}
 	}
 
+	/* On incrémente le nombre d'échantillon reçu depuis le début de la période */
 	params->nbEch++;
+
 	return doCalculation;
 }
 
+/* Calcul le poul */
 int calculatePoul(s_MesureParams* params) {
+	/* On considère qu'on reçoit un échantillon toutes les 2ms, on a donc la relation poul = 60/(nbEch*2e-3) = 30000/nbEch */
 	return 30000/params->nbEch;
 }
 
+/* Calcul le pourcentage de saturation en oxygène du sang */
 int calculateSpO2(s_MesureParams* params, absorp* myAbsorp) {
-	float ptpAcr = params->maxAcr - params->minAcr;
-	float ptpAcir = params->maxAcir - params->minAcir;
+	float ptpAcr = params->maxAcr - params->minAcr; /* Hauteur peek to peek d'acr */
+	float ptpAcir = params->maxAcir - params->minAcir; /* Hauteur peek to peek d'acir */
 
-	float RsIR = (ptpAcr*myAbsorp->dcir)/(ptpAcir*myAbsorp->dcr);
+	float RsIR = (ptpAcr*myAbsorp->dcir)/(ptpAcir*myAbsorp->dcr); /* Coefficient d'absorption */
 
 	if(RsIR < 0.4) {
+		/* Si RsIR < 0.4, il y a probablement une erreur dans les données, on renvoit 0*/
 		return 0;
 	} else if(RsIR < 1) {
+		/* Si RsIR est compris entre 0.4 et 1, on approxime selon une portion de droite */
 		return 110 - 25*RsIR;
 	} else {
+		/* Si RsIR est supérieur à 1, on approxime selon une autre portion de droite */
 		return 118 - 100*RsIR/3;
 	}
 }
 
+/* Effectue la mesure du poul et du SpO2 */
 oxy mesure(s_MesureParams* params, absorp* myAbsorp) {
 	oxy myOxy;
 
+	/* Calcul le poul et le SpO2 */
 	myOxy.spo2 = calculateSpO2(params, myAbsorp);
 	myOxy.pouls = calculatePoul(params);
 
+	/* Réinitialise les paramètres pour la prochaine période */
 	params->nbEch = 0;
 	params->minAcr = 0;
 	params->minAcir = 0;
@@ -88,33 +113,42 @@ oxy mesure(s_MesureParams* params, absorp* myAbsorp) {
 	return myOxy;
 }
 
+/* Libère la mémoire allouée pour la structure params de mesure */
 void closeMesure(s_MesureParams* params) {
 	free(params);
 }
 
+/* Fonction de test des fonctions de mesure */
 oxy mesureTest(char* filename){
-	int etat = 0;
-	int redoMesure = 0;
+	int etat = 0; /* Etat de lecture du fichier */
+	int redoMesure = 0; /* Vaut 1 si une période vient de se finir */
 	absorp myAbsorp;
 	oxy myOxy;
 
+	/* Initialisation des paramètres et ouverture du fichier */
 	s_MesureParams* params = initMesure();
 	FILE* file = initFichier(filename);
 	
 	while (etat != EOF) {
+		/* Tant que le fichier n'est pas fini on récupère les données */
 		myAbsorp = lireFichier(file, &etat);
 
+		/* On vérifie qu'on ne vient pas de lire après la dernière ligne */
 		if (etat != EOF) {
+			/* On vérifie si une période vient de se terminer */
 			redoMesure = checkPicPosition(params, &myAbsorp);
 
 			if(redoMesure) {
+				/* Si c'est le cas, on fait la mesure du poul et de SpO2 */
 				myOxy = mesure(params, &myAbsorp);
 			}
 		}
 	}
 	
+	/* Fermeture du fichier et désallocation des paramètres */
 	closeMesure(params);
 	finFichier(file);
 
+	/* Dernières valeurs calculées */
 	return myOxy;
 }
--- mesure.h
+++ mesure.h
@@ -1,32 +1,142 @@
+/*!
+ * \file mesure.h
+ * \brief Fonctions liées à la mesure du poul et du SpO2
+ * \author Thomas BRAGUE (thomas.brague@isen-ouest.yncrea.fr)
+ * \author Alexandre THOMAS (alexandre.thomas@isen-ouest.yncrea.fr)
+ * \version 1
+ * \date 31/03/2020
+ *
+ */
+
 #ifndef MESURE_H
 #define MESURE_H
 
 #include "define.h"
 #include "fichiers.h"
 
+/*! 
+ * \enum e_EtatMesure
+ * \brief Enumeration contenant les états de mesure de la courbe
+ */
 typedef enum e_EtatMesure {
-    PIC_HAUT, PIC_BAS
+    PIC_HAUT, /*!< Actuellement sur un pic haut (supérieur à 0) */
+    PIC_BAS /*!< Actuellement sur un pic bas (inférieur à 0) */
 } e_EtatMesure;
 
+/*! 
+ * \struct s_MesureParams
+ * \brief Structure contenant les paramètres pour les fonctions de mesure
+ */
 typedef struct s_MesureParams {
-    float minAcr, maxAcr;
-    float minAcir, maxAcir;
-    int nbEch;
+    float minAcr, maxAcr; /*!< Minimum et maximum d'acr sur la période courante */
+    float minAcir, maxAcir; /*!< Minimum et maximum d'acir sur la période courant */
+    int nbEch; /*!< Nombre d'échantillon traités depuis le début de la période */
 
-    e_EtatMesure etat;
+    e_EtatMesure etat; /*!< Etat de la période actuelle */
 } s_MesureParams;
 
-/** 
- * Fonctions
+/*! 
+ * \fn float maxMesure(float a, float b)
+ * \brief Retourne le maximum entre a et b
+ * 
+ * \param a Un nombre flottant quelconque
+ * \param b Un nombre flottant quelconque
+ * 
+ * \return Le maximum de {a,b}
  */
 float maxMesure(float a, float b);
+
+/*! 
+ * \fn float minMesure(float a, float b)
+ * \brief Retourne le minimum entre a et b
+ * 
+ * \param a Un nombre flottant quelconque
+ * \param b Un nombre flottant quelconque
+ * 
+ * \return Le minimum de {a,b}
+ */
 float minMesure(float a, float b);
+
+/*! 
+ * \fn s_MesureParams* initMesure()
+ * \brief Initialise les paramètres utilisée par les fonctions de mesure
+ * 
+ * \return Un pointeur sur une sructure s_MesureParams allouée dynamiquement
+ * 
+ * Initialise toutes les valeurs de la structure à 0 et l'etat à "PIC_HAUT"
+ */
 s_MesureParams* initMesure();
-int checkPicPosition(s_MesureParams* params, absorp* absorp);
+
+/*! 
+ * \fn int checkPicPosition(s_MesureParams* params, absorp* myAbsorp)
+ * \brief Modifie la structure params en fonction de l'avancée dans la courbe d'absorption
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres de mesure
+ * \param myAbsorb Un pointeur sur la structure des valeurs d'absorption
+ * 
+ * \return 1 si une période vient de se terminer, 0 sinon
+ * 
+ * Incremente le compteur d'échantillon à chaque mesure et regarde s'il existe un nouveau
+ * maximum sur cette période. Une période des considérée finie lorsque acr est devenu
+ * négative alors que l'état était "PIC_HAUT" puis qu'acr est devenu positif alors que l'état était "PIC_BAS"
+ */
+int checkPicPosition(s_MesureParams* params, absorp* myAbsorp);
+
+/*! 
+ * \fn int calculatePoul(s_MesureParams* params)
+ * \brief Calcul le poul
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres de mesure
+ * 
+ * \return Le poul arrondi à l'entier inférieur
+ * 
+ * Calcul le poul en sachant qu'un échantillon est reçu toute les 2ms
+ */
 int calculatePoul(s_MesureParams* params);
+
+/*! 
+ * \fn int calculateSpO2(s_MesureParams* params, absorp* myAbsorp)
+ * \brief Calcul le pourcentage de saturation en oxygène du sang
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres de mesure
+ * \param myAbsorb Un pointeur sur la structure des valeurs d'absorption
+ * 
+ * \return Le pourcentage de saturation en oxygène du sang (SpO2) arrondi à l'entier inférieur
+ * 
+ * Calcul le SpO2 en faisant une approximation de droite entre 0.4 et 1, et entre 1 et 3
+ */
 int calculateSpO2(s_MesureParams* params, absorp* myAbsorp);
+
+/*! 
+ * \fn oxy mesure(s_MesureParams* params, absorp* myAbsorp)
+ * \brief Effectue la mesure du poul et du SpO2
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres de mesure
+ * \param myAbsorb Un pointeur sur la structure des valeurs d'absorption
+ * 
+ * \return Une structure oxy contenant le poul et le SpO2 de la période précédente
+ * 
+ * Effectue la mesure du poul et du SpO2 et réinitialise les valeurs de params pour
+ * la prochaine période
+ */
 oxy mesure(s_MesureParams* params, absorp* myAbsorp);
+
+/*! 
+ * \fn void closeMesure(s_MesureParams* params);
+ * \brief Libère la mémoire allouée pour la structure params de mesure
+ * 
+ * \param params Un pointeur sur la structure contenant les paramètres de mesure
+ */
 void closeMesure(s_MesureParams* params);
+
+/*! 
+ * \fn absorp mesureTest(char* filename);
+ * \brief Fonction de test des fonctions de mesure
+ * 
+ * \param filename Le nom du fichier à lire contenant les valeurs à traiter
+ * 
+ * \return La dernière valeur d'oxy calculée
+ */
 oxy mesureTest(char* filename);
 
 #endif /* MESURE_H */
